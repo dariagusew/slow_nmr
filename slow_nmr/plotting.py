d56ed9a88e19b467040421b724b079870d703a8b
zvh378 <zvh378@sbinlabcomp01fl.unicph.domain>
2024-12-02 15:02:35 +0100

    create new dir

---
+++ slow_nmr/plotting.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from slow_nmr.utils import traj_loader, energy1d,  q_to_chem_shift_mapping
+from slow_nmr.autocorelation import calc_acf
+import scienceplots
+plt.style.use(['science','no-latex'])
+
+
+def plot_traj(traj_path,dt, stride, save_fig ='no'):
+    time, traj  = traj_loader(traj_path, dt, stride)
+
+    plt.figure(figsize=(4,2),dpi=300)
+    plt.plot(time, traj)
+    plt.xlabel(r'$t/10^7$',fontsize=15)
+    plt.ylabel(r'$S_{path}$',fontsize=15)
+    if save_fig == 'yes':
+        plt.savefig('traj_path.pdf',dpi=300)
+    plt.show()
+
+def plot_chem_shift(traj_path,ref_path, dt, w_mean_path, stride,n_steps,  save_fig ='no'):
+    
+    chem_shift = q_to_chem_shift_mapping(traj_path, ref_path, w_mean_path, dt, stride)
+    time = np.linspace(0, n_steps*dt*stride, n_steps)
+
+    plt.figure(figsize=(4,2),dpi=300)
+    plt.plot(time, chem_shift)
+    plt.xlabel(r'$t$',fontsize=15)
+    plt.ylabel(r'$<\delta> [ppm]$',fontsize=15)
+    if save_fig == 'yes':
+        plt.savefig('chem_shift_path.pdf',dpi=300)
+    plt.show()
+
+def plot_energy(ref_path, traj_path, dt, stride, save_fig='no'):
+
+    ref = np.load(ref_path)
+    rc = ref[:,0]
+    fes = ref[:,1]
+    _, traj = traj_loader(traj_path, dt, stride)
+    bins, energy = energy1d(traj, bins=100)
+    
+
+    plt.figure(dpi=300)
+    plt.plot(rc,(fes-np.min(fes)),c='k',label= 'MD')
+    plt.plot(bins[1:],energy[1],label= 'LD')
+    plt.xlabel('S$_{path}$',fontsize=15)
+    plt.ylabel('Free energy [kJ/mol]',fontsize=15)
+    plt.legend(frameon = True, bbox_to_anchor=(1, 0, 0, 1))
+    if save_fig == 'yes':
+        plt.savefig('energy.pdf',dpi=300)
+    plt.show()
+
+def plot_acf(ob_idx, traj_path, chem_shift_path, dt, stride, corrdim, corrstride, save_fig ='no'):
+
+    tauaxis, ACF = calc_acf(ob_idx, traj_path, chem_shift_path, dt, stride, corrdim, corrstride)
+
+    plt.figure(dpi=300)
+    plt.plot(tauaxis, ACF)
+    plt.ylabel(r'$C(\tau)$',fontsize=15)
+    plt.xlabel(r'$\tau$',fontsize=15)
+    if save_fig == 'yes':
+        plt.savefig('acf.pdf',dpi=300)
+    plt.show()
---
+++ slow_nmr/utils.py
@@ -0,0 +1,89 @@
+import numpy as np 
+import pandas as pd
+import mdtraj as md 
+from numba import njit
+from scipy.optimize import curve_fit
+from slow_nmr.autocorelation import ACFfft, threeexp
+
+@njit
+def traj_loader(traj_path, dt, stride):
+    
+    traj  = np.load(traj_path)[::stride]
+    time = np.linspace(0,len(traj)*dt*stride, len(traj))
+
+    return time, traj 
+
+@njit
+def energy1d(traj, bins, T=298, R = 8.314*10**(-3)):
+    
+    hist, bins = np.histogram(traj,bins=bins,density=True)
+    energy = -R*T*np.log(hist)
+    energy = energy - np.min(energy)
+    
+    return bins, energy
+
+def chem_shift_mean_maker(path_w, md_traj, md_top, ref_path,rc_path, res_idx, save = 'yes'):
+
+    top = md.load(md_top)
+    traj = md.load(md_traj,top=top)
+
+    ref = np.load(ref_path)
+    rc = np.genfromtxt(rc_path)
+    
+    fes_dt = ref[:,0][1] - ref[:,0][0]
+    
+    chem_shifts = pd.read_pickle(path_w)
+    
+    chem_shift_res = np.zeros(traj.n_frames)
+    
+    for frame in range(traj.n_frames):
+        chem_shift_res[frame]=chem_shifts[frame][res_idx]['CA']
+    
+    idx = []
+    for i in range(len(ref[:,0])):
+        idx.append(np.where((rc[:,1]<ref[:,0][i]) & (rc[:,1]>=ref[:,0][i]-fes_dt)))
+    
+    w_mean_res = []
+    for i in range(len(idx)):
+        w_mean_res.append(np.mean(chem_shift_res[idx[i]]))
+
+    w_mean_res = np.array(w_mean_res)
+    
+    if save=='yes':
+        np.save("w_mean_res", w_mean_res)
+
+    return w_mean_res
+
+@njit
+def q_to_chem_shift_mapping(traj_path, ref_path, w_mean_path, dt, stride, save='no'):
+
+    ref = np.load(ref_path)
+    fes_dt = ref[:,0][1] - ref[:,0][0]
+
+    _, traj = traj_loader(traj_path, dt, stride)
+    
+    idxq = []
+    for i in range(len(ref[:,0])):
+        idxq.append(np.where((traj<ref[:,0][i]) & (traj>=ref[:,0][i]-fes_dt)))
+    
+    w_mean = np.load(w_mean_path)
+    
+    chem_shift = np.zeros(traj.shape)
+    
+    for i in range(201):
+        chem_shift[idxq[i]] = w_mean[i]
+    
+    if save=='yes':
+        np.save(chem_shift,"chem_shift")
+    
+    return chem_shift
+
+
+
+def R_CPMG(pars,tau_ex,tau_cpmg):
+    
+    Rex = pars[0]*pars[1]+pars[2]*pars[3]+pars[4]*pars[5]
+
+    R_cpmg = Rex*(1-(tau_ex/tau_cpmg)*np.tanh(tau_cpmg/tau_ex))
+    
+    return R_cpmg, Rex
